684037723e4e6ea623a67113093e613453f1d320
Telmo1996 <tfercor@gmail.com>
2020-08-25 16:53:47 +0200

    rofi theme tweak y vim

--- installScript.sh
+++ installScript.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get install rofi i3blocks lxappearance compton feh
+sudo apt-get install rofi i3blocks lxappearance compton feh vim
 #para la bateria: acpi
 sudo apt-get install alacritty
