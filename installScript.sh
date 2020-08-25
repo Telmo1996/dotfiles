89762d7751bc97171a06a5154c7182a043419b49
Telmo1996 <tfercor@gmail.com>
2020-08-25 23:07:15 +0200

    color flexing

--- installScript.sh
+++ installScript.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 sudo apt-get install rofi i3blocks lxappearance compton feh vim
 #para la bateria: acpi
+
 sudo apt-get install alacritty
+
+#scripts wapos pa flexear colores
+git clone https://github.com/stark/color-scripts
