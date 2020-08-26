350f0414a9fee3022c1d67a67c1a9cc291a84bec
Telmo1996 <tfercor@gmail.com>
2020-08-26 17:54:47 +0200

    Spotify

--- installScript.sh
+++ installScript.sh
@@ -2,6 +2,11 @@
 sudo apt-get install rofi i3blocks lxappearance compton feh vim
 #para la bateria: acpi
 
+#para playerctl en ubuntu (i3blocks/mediaplayer)
+wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/libplayerctl2_2.0.1-1_amd64.deb
+wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/playerctl_2.0.1-1_amd64.deb
+sudo dpkg -i libplayerctl2_2.0.1-1_amd64.deb playerctl_2.0.1-1_amd64.deb
+
 sudo apt-get install alacritty
 
 #scripts wapos pa flexear colores
