9bc33b3c0257ab2465a6b3a12dbfdeb1b7abbf3a
Telmo1996 <tfercor@gmail.com>
2020-10-25 11:38:03 +0100

    vim tabs y brillo

--- .vimrc
+++ .vimrc
@@ -2,8 +2,8 @@ syntax on
 
 set tabstop=4 softtabstop=4
 set shiftwidth=4
-set expandtab
-set smartindent
+set noexpandtab
+set autoindent
 set nu
 set nowrap
 set noswapfile
--- brillo.sh
+++ brillo.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo $1 | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+if [ -z "$1" ]; then
+    cat /sys/class/backlight/amdgpu_bl0/brightness
+else
+    echo $1 | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+fi
